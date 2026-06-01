d097b7f1cf1f4306842cb12d2ef8e851fc785a05
Thando Calana <110775561+ThandoCalana@users.noreply.github.com>
2026-06-01 17:15:39 +0200

    added video id extraction function that looks at the videos in a playlist, and returns a list of video IDs

--- video_stats.py
+++ video_stats.py
@@ -32,12 +32,51 @@ def get_playlist_id():
         channel_items = data["items"][0]
         channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        print(channel_playlist_id)
+        # print(channel_playlist_id)
         return channel_playlist_id
 
     except requests.exceptions.RequestException as e:
         raise e
     
+def get_video_ids(playlist_id): # extract a list of video IDs from the provided playlist
+
+    video_ids = []
+    page_token = None # Initialize to None since the first run will have no page token
+
+    max_results = 50
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}'
+
+
+    try:
+        while True: # Keep looping until there are no more page tokens
+
+            url = base_url
+
+            if page_token:
+                url += f'&pageToken={page_token}' # concat page token to url once the page token var exists (2nd run onwards)
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            page_token = data.get('nextPageToken')
+
+            if not page_token:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+    
 # Only true if the script is run directly instead of as a module
 if __name__ == "__main__":
-    get_playlist_id()
+    playlist_id = get_playlist_id()
+    print(get_video_ids(playlist_id))
