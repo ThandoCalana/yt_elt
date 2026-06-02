16685af621b6d9acc044d79b75868d770e2bf1ff
Thando Calana <110775561+ThandoCalana@users.noreply.github.com>
2026-06-02 15:48:08 +0200

    Added extraction function which extracts details about each video then stores them in a list

--- video_stats.py
+++ video_stats.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path="./.env")
 # Store sensitive data in a .env file
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = 'MrBeast'
+MAX_RESULTS = 50
 
 # Enclose code you want to repeat in a function - Software dev best practices
 def get_playlist_id():
@@ -43,8 +44,7 @@ def get_video_ids(playlist_id): # extract a list of video IDs from the provided 
     video_ids = []
     page_token = None # Initialize to None since the first run will have no page token
 
-    max_results = 50
-    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}'
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={MAX_RESULTS}&playlistId={playlist_id}&key={API_KEY}'
 
 
     try:
@@ -75,8 +75,55 @@ def get_video_ids(playlist_id): # extract a list of video IDs from the provided 
     except requests.exceptions.RequestException as e:
         raise e
 
+
+def extract_video_details(video_ids):
+    
+    extracted_details = []
+
+    def batch_videos(video_id_list:list, batch_size:int):
+        for video_id in range(0, len(video_id_list), batch_size): # start, end, step
+            # Use yield to still return the result but don't stop the looping process
+            yield video_id_list[video_id: video_id + batch_size] # extracting a slice of the video IDs
+
+    try:
+        for batch in batch_videos(video_ids, MAX_RESULTS):
+            video_ids_str = ",".join(batch) # API takes a comma-separated list of video ids 
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                content_details = item['contentDetails']
+                video_statistics = item['statistics']
+                video_snippet = item['snippet']
+
+                extracted_data = {
+                    'video_id': video_id,
+                    'duration': content_details['duration'],
+                    'published_at': video_snippet['publishedAt'],
+                    'title': video_snippet['title'],
+                    'likes': video_statistics.get('likeCount', None), # Some videos may not have likes -> default to None
+                    'comments': video_statistics.get('commentCount', None),
+                    'views': video_statistics.get('viewCount', None)
+                    }
+                
+                extracted_details.append(extracted_data)
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+    return extracted_details
+            
+
+
     
 # Only true if the script is run directly instead of as a module
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
-    get_video_ids(playlist_id)
+    video_ids = get_video_ids(playlist_id)
+    extract_video_details(video_ids)
